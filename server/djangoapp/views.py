40e33af397078b997ecc1b083b7c802b3c7e50d7
albert <albertwelong@gmail.com>
2024-11-06 23:46:50 -0500

    python files lint fix

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,8 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments
+# from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
@@ -125,7 +126,7 @@ def get_dealer_details(request, dealer_id):
 
 def add_review(request):
     if (request.user.is_anonymous is False):
-        data = json.loads(request.body)
+        # data = json.loads(request.body)
         try:
             # response = post_review(data)
             return JsonResponse({"status": 200})
