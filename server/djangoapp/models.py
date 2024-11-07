76f93764c4698df12473172f3c561f95886b69ff
albert <albertwelong@gmail.com>
2024-11-06 23:16:44 -0500

    lint fix models and populate in djangoapp

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -30,6 +30,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
