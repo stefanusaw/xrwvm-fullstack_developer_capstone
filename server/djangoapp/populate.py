c0142ea74af8a2f224f719c5d8dc37671d9dd6db
albert <albertwelong@gmail.com>
2024-11-06 23:26:15 -0500

    lint fix populate and restapis djangoapp

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,9 +12,12 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],
-                                                         description=data['description']))
-
+        car_make_instances.append(
+            CarMake.objects.create(
+                name=data['name'],
+                description=data['description']
+            )
+        )
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -48,7 +51,6 @@ def initiate():
        "year": 2023, "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[4]},
-        # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
